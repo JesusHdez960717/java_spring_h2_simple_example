34aa28b5a74b3d771ea3f6c25bee1e33ec8c4d37
JHB <jhernandezb96@gmail.com>
2022-04-11 22:55:30 -0400

    Misc

--- src/main/java/hugo_test/Main.java
+++ src/main/java/hugo_test/Main.java
@@ -9,29 +9,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping(value = "/hugo_test")
 public class Main extends SpringBootServletInitializer {
 
-    private static ConfigurableApplicationContext context;
-
     public static void main(String[] args) {
-        context = SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Main.class);
-    }
-
-    @GetMapping("/hi")
-    public String hi() {
-        return "Hello Hugo";
     }
 }
--- src/main/java/hugo_test/validation/Validation.java
+++ src/main/java/hugo_test/validation/Validation.java
@@ -1,6 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright 2022 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package hugo_test.validation;
 
@@ -11,6 +23,7 @@ import javax.validation.Validator;
 
 /**
  *
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
  * @author JesusHdezWaterloo@Github
  */
 public class Validation {
